4df4933fb02845ccf46f3eee48b7ea0bca4e0565
root <root@roman.intelligentcompute.com>
2018-04-13 22:53:10 +0100

    build.sh improved

--- build.sh
+++ build.sh
@@ -7,4 +7,4 @@ sed -ri 's,(CONFIG_RTE_LIBRTE_PMD_PCAP=).*,\1y,' .config
 sed -ri 's,(CONFIG_RTE_LIBRTE_MLX5_PMD=).*,\1y,' .config
 cd ..
 make -j7 install T=x86_64-native-linuxapp-gcc EXTRA_CFLAGS="-fPIC" DESTDIR=/usr/local && echo "success" || echo "fail"
-
+ldconfig
